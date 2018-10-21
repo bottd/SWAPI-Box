079c4fd9c34b46fce8c35d2a02852ce9d6458a81
Drake Bott <bott.da@gmail.com>
2018-10-21 13:34:54 -0600

    Updates

---
+++ precache-manifest.c0258f99d08251f54327a514622c9bc1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "a312f81ecbf659e8662994cbb96a52a4",
+    "url": "/SWAPI-Box/static/media/stars-bg.a312f81e.jpg"
+  },
+  {
+    "revision": "fbf57569107072436ddcd5ef61568f11",
+    "url": "/SWAPI-Box/static/media/obi-wan.fbf57569.jpg"
+  },
+  {
+    "revision": "cc3019aa6439ef893839b8a0d6d0391d",
+    "url": "/SWAPI-Box/static/media/Starjedi.cc3019aa.ttf"
+  },
+  {
+    "revision": "92940b4e8c42006a4786",
+    "url": "/SWAPI-Box/static/js/runtime~main.92940b4e.js"
+  },
+  {
+    "revision": "d1ef4488c380092f9e1c",
+    "url": "/SWAPI-Box/static/js/main.d1ef4488.chunk.js"
+  },
+  {
+    "revision": "bc91389440fadc5ce05d",
+    "url": "/SWAPI-Box/static/js/1.bc913894.chunk.js"
+  },
+  {
+    "revision": "d1ef4488c380092f9e1c",
+    "url": "/SWAPI-Box/static/css/main.af12a2f7.chunk.css"
+  },
+  {
+    "revision": "9eaca5279120c7e8b18da37762106440",
+    "url": "/SWAPI-Box/index.html"
+  }
+];
